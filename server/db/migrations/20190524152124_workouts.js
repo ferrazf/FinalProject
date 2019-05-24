f7a2d4e70cb304db198fdb761062ff7f93614939
FerrazF <ferrazf9@gmail.com>
2019-05-24 17:32:31 -0400

    Create users initial table migration

---
+++ server/db/migrations/20190524152124_workouts.js
@@ -0,0 +1,24 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("workouts", function(table) {
+    table
+      .increments("id")
+      .unsigned()
+      .notNullable()
+      .index();
+    table
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .index();
+    table.boolean("completed").defaultTo(false);
+    table
+      .date("last_updated")
+      .notNullable()
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+    table.foreign("user_id").references("users.id");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable("workouts");
+};
