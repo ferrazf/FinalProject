35cf8321a5485c24344299453187fe9b8c18ca90
FerrazF <ferrazf9@gmail.com>
2019-05-24 19:04:41 -0400

    Add exercises table migration

---
+++ server/db/migrations/20190524185023_create_exercises_table.js
@@ -0,0 +1,23 @@
+//Create 'exercises' table
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("exercises", function(table) {
+    table
+      .increments("id")
+      .unsigned()
+      .notNullable()
+      .index();
+    table
+      .increments("muscle_group_id")
+      .unsigned()
+      .notNullable()
+      .index();
+    table.string("name");
+    table.string("descr");
+    table.foreign("muscle_group_id").references("muscle_groups.id");
+  });
+};
+
+//Drop 'exercises' table
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable("exercises");
+};
