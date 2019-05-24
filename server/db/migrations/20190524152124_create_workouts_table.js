fe0d140c1a3e0366cea03f117cf752155e14c660
FerrazF <ferrazf9@gmail.com>
2019-05-24 18:36:53 -0400

    Add workouts table migration

---
+++ server/db/migrations/20190524152124_create_workouts_table.js
@@ -0,0 +1,29 @@
+//Create 'workouts' table
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("workouts", function(table) {
+    table
+      .increments("id")
+      .unsigned()
+      .notNullable()
+      .index();
+    table
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .index();
+    table
+      .boolean("completed")
+      .notNullable()
+      .defaultTo(false);
+    table
+      .date("last_updated")
+      .notNullable()
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+    table.foreign("user_id").references("users.id");
+  });
+};
+
+//Drop 'workouts' table
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable("workouts");
+};
