f5db2beb9c392b36d683bdb4930a40d9d8bd3e97
FerrazF <ferrazf9@gmail.com>
2019-05-24 18:33:53 -0400

    Add users table migration

--- server/db/migrations/20160712210838_create_users_table.js
+++ server/db/migrations/20160712210838_create_users_table.js
@@ -1,14 +1,17 @@
+//Create 'users' table
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("users", function(table) {
     table
       .increments("id")
       .unsigned()
+      .notNullable()
       .index();
     table.string("name");
     table.string("email");
   });
 };
 
+//Drop 'users' table
 exports.down = function(knex, Promise) {
   return knex.schema.dropTable("users");
 };
