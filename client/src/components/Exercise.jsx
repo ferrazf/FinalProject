a8565ee259953f7ae51b83d694f83f100d3ca4d1
vshibukawa <viviane.shibukawa@gmail.com>
2019-05-25 18:50:30 -0400

    add style to 'add exercise'

--- client/src/components/Exercise.jsx
+++ client/src/components/Exercise.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import { FormField, TextInput, Button } from 'grommet';
+
 
 export default( { handleFormSubmit } ) => (
   <form onSubmit={handleFormSubmit}>
-    <label>
-      Muscle:
-      <input type="text" name="muscle" />
-    </label>
-    <label>
-      Name:
-      <input type="text" name="name" />
-    </label>
-    <label>
-      Desc:
-      <input type="text" name="descr" />
-    </label>
-    <button>Submit</button>
+    <FormField label="Muscle:">
+      <TextInput type="text" name="muscle" placeholder="muscle" />
+    </FormField>
+    <FormField label="Name:">
+      <TextInput type="text" name="name" placeholder="name" />
+    </FormField>
+    <FormField label="Description:">
+      <TextInput type="text" name="descr" placeholder="description" />
+    </FormField>
+    <Button type="submit" primary label="Submit"/>
   </form>
 )
