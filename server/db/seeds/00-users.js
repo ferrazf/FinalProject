0a4c024da8ffa356037adbc8623e833ba80a17fd
FerrazF <ferrazf9@gmail.com>
2019-05-25 21:00:54 -0400

    Add users table seed, fix table migrations

--- server/db/seeds/00-users.js
+++ server/db/seeds/00-users.js
@@ -1,12 +1,33 @@
 exports.seed = function(knex, Promise) {
-  return knex('users').del()
-    .then(function () {
+  return knex("users")
+    .del()
+    .then(function() {
       return Promise.all([
-        knex('users').insert({ name: 'Alice Bobbington', email: 'alice.bobbington@alice.com'}),
-        knex('users').insert({ name: 'Bob Charles', email: 'bob.charles@charles.com'}),
-        knex('users').insert({ name: 'Mary McMaster', email: 'marymcm@mary.net' }),
-        knex('users').insert({ name: 'Charlie Smith', email: 'charlie.smith@smith.com' }),
-        knex('users').insert({ name: 'Alex Simpson', email: 'alex.simpson@alex.com' })
+        knex("users").insert({
+          id: 1,
+          name: "Alice Bobbington",
+          email: "alice.bobbington@alice.com"
+        }),
+        knex("users").insert({
+          id: 2,
+          name: "Bob Charles",
+          email: "bob.charles@charles.com"
+        }),
+        knex("users").insert({
+          id: 3,
+          name: "Mary McMaster",
+          email: "marymcm@mary.net"
+        }),
+        knex("users").insert({
+          id: 4,
+          name: "Charlie Smith",
+          email: "charlie.smith@smith.com"
+        }),
+        knex("users").insert({
+          id: 5,
+          name: "Alex Simpson",
+          email: "alex.simpson@alex.com"
+        })
       ]);
     });
 };
