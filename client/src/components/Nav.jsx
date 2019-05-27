64e08dad96f3d1f4660d17703b3d4180938d3989
FerrazF <ferrazf9@gmail.com>
2019-05-27 19:19:49 -0400

    Fix pull issue

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -10,9 +10,9 @@ function Nav(props) {
         <Router>
             <Box fill= "horizontal">
                 <AppBar >
-                <Link to="/" justify='start'>Pocket Spotter</Link>
-                <Link to="/profile"><Heading level='3' margin='none'> Profile</Heading></Link>
-                <Text>{props.name}</Text> 
+                    <Link to="/" justify='start'>Pocket Spotter</Link>
+                    <Link to="/profile"><Heading level='3' margin='none'> Profile</Heading></Link>
+                    <Text>{props.name}</Text>
                 </AppBar>
             </Box>
             <Route exact path="/" component={() => <Home workouts={props.workouts}/>} />
