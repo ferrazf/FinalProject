3786d29c36be479d38a3f14b0b76115a13120bf3
vshibukawa <viviane.shibukawa@gmail.com>
2019-05-28 18:54:10 -0400

    add get api/muscles/:muscleId/exercises

--- server/routes/muscles.js
+++ server/routes/muscles.js
@@ -6,10 +6,14 @@ const router  = express.Router();
 module.exports = (knex) => {
   const middleware  = require('../middleware/index')(knex);
   const helpers     = require('../helpers/muscles')(knex);
+  const exercisesHelpers     = require('../helpers/exercises')(knex);
 
   // mucsles
   router.route("/")
         .get(helpers.getMuscles)
+
+  router.route("/:muscleId/exercises")
+        .get(exercisesHelpers.getExercises)
 
   return router;
 }
