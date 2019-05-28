3997ab392e49521079675bd5a7afbd7a168c88f5
FerrazF <ferrazf9@gmail.com>
2019-05-28 19:43:38 -0400

    Add Icons package, update button styles

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,26 +1,34 @@
 import React from "react";
-import { Box, Button, Form, FormField, Grommet, Heading, Paragraph} from 'grommet';
-import { hpe } from 'grommet-theme-hpe';
+import {
+  Box,
+  Button,
+  Form,
+  FormField,
+  Grommet,
+  Heading,
+  Paragraph
+} from "grommet";
+import { hpe } from "grommet-theme-hpe";
 
 function Register(props) {
-    //props.register(user)
-    return (
-        <Grommet theme={hpe}>
-            <Box align="center" background="status-ok">
-                <p>Register</p>
-            </Box>
-            <Box pad="medium" border={{
-                "color": "border",
-                "side": "all"
-            }}>
-                <Form>
-                    <FormField name="name" label="Name" />
-                    <FormField name="email" label="Email Address" />
-                    <Button type="submit" primary label="Register" />
-                </Form>
-            </Box>
-        </Grommet>
-    )
+  //props.register(user)
+  return (
+    <Grommet theme={hpe}>
+      <Box align="center" background="status-ok">
+        <p>Register</p>
+      </Box>
+      <Box pad="medium">
+        <Form>
+          <FormField name="name" label="Name" />
+          <FormField name="email" label="Email Address" />
+          <Box align="center">
+            {" "}
+            <Button type="submit" primary label="Register" />
+          </Box>
+        </Form>
+      </Box>
+    </Grommet>
+  );
 }
 
 export default Register;
--- client/src/components/workout/Index.jsx
+++ client/src/components/workout/Index.jsx
@@ -1,46 +1,58 @@
-import React from 'react';
-import { Text, Box, Button, Icons } from 'grommet';
+import React from "react";
+import { Box, Button, Grommet, Text } from "grommet";
 import { Link } from "react-router-dom";
+import { hpe } from "grommet-theme-hpe";
+import { Edit, FormView } from "grommet-icons";
 
 function Workout(props) {
-    const start = (!props.workout.started_at) && (
-            <Button name={props.workout.workout_id}
-                alignSelf= "end"
-                label="start"
-                onClick={props.handleStart}
-            />
-        )
-    const finish = (props.workout.started_at && !props.workout.finished_at) && (
-            <Button name={props.workout.workout_id}
-                alignSelf= "end"
-                label="finish"
-                onClick={props.handleFinish}
-            />
-        )
+  const start = !props.workout.started_at && (
+    <Button
+      primary
+      name={props.workout.workout_id}
+      alignSelf="end"
+      label="start"
+      onClick={props.handleStart}
+    />
+  );
+  const finish = props.workout.started_at && !props.workout.finished_at && (
+    <Button
+      name={props.workout.workout_id}
+      alignSelf="end"
+      label="finish"
+      onClick={props.handleFinish}
+    />
+  );
 
-    return(
-        <Box  pad="medium" border= {{
-            "color": "border",
-            "side": "all"
-        }}>
-            <Text > {props.workout.name.join(', ')} </Text>
-            <Link to="/edit" >
-                <Button
-                    alignSelf= "end"
-                    label="edit"
-                    onClick={() => {}}
-                />
-            </Link>
-            <Link to="/workout" >
-                <Button
-                    alignSelf= "end"
-                    label="view"
-                    onClick={() => {}}
-                />
-            </Link>
-            {start}
-            {finish}
-        </Box>
-    )
+  return (
+    <Grommet theme={hpe}>
+      <Box
+        pad="medium"
+        border={{
+          color: "border",
+          side: "all"
+        }}
+      >
+        <Text> {props.workout.name.join(", ")} </Text>
+        <Link to="/edit">
+          <Button
+            icon={<Edit />}
+            alignSelf="end"
+            label="edit"
+            onClick={() => {}}
+          />
+        </Link>
+        <Link to="/workout">
+          <Button
+            icon={<FormView />}
+            alignSelf="end"
+            label="view"
+            onClick={() => {}}
+          />
+        </Link>
+        {start}
+        {finish}
+      </Box>
+    </Grommet>
+  );
 }
 export default Workout;
