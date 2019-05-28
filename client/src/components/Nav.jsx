36d7631c42fce0b07edaebb6a262c6f1ab457981
FerrazF <ferrazf9@gmail.com>
2019-05-28 16:35:13 -0400

    Resolve git push issue

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Box, Heading, Text} from 'grommet';
+import { Box, Heading, Text } from 'grommet';
 import { Link } from "react-router-dom";
 
 
 function Nav(props) {
     return (
-            <Box fill= "horizontal">
-                <AppBar >
-                    <Link to="/" justify='start'>Pocket Spotter</Link>
-                    <Link to="/profile"><Heading level='3' margin='none'> Profile</Heading></Link>
-                    <Text>{props.name}</Text>
-                </AppBar>
-            </Box>
+        <Box fill="horizontal">
+            <AppBar >
+                <Link to="/" justify='start'> Pocket Spotter</Link>
+                <Link to="/profile"><Heading level='3' margin='none'> Profile</Heading></Link>
+                 {/* Replace with logout if user logged in */}
+                <Link to="/register"> Register</Link>
+                <Text>{props.name}</Text>
+            </AppBar>
+        </Box>
     );
 }
 
 const AppBar = (props) => (
-      <Box
+    <Box
         tag='header'
         direction='row'
         align='center'
