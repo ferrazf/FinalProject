2b0ed8db6f8e3763bcadbc546b47e6fbcd38854c
FerrazF <ferrazf9@gmail.com>
2019-05-30 10:46:30 -0400

    Add template styling to exercise display component

--- client/src/components/exercise/Display.jsx
+++ client/src/components/exercise/Display.jsx
@@ -1,35 +1,51 @@
-import { Box, Button, Grommet, Text } from 'grommet';
+import { Box, Button, Grommet, Heading, Text } from 'grommet';
 import React from 'react';
 import { Link } from "react-router-dom";
 import { hpe } from "grommet-theme-hpe";
-import { Edit } from "grommet-icons";
+import { Edit, PlayFill } from "grommet-icons";
 
 //Displays exercises in workoutDisplay Container
 //passes exercise prop through callback inorder to allow edit
 function exercise_display(props) {
   return (
     <Grommet theme={hpe}>
-      <Box pad="medium"
+      <Box margin="medium"
         border={{
           color: "border",
           side: "bottom"
         }}>
-        <Text> {props.exercise.name} </Text>
-        <Text>Sets: {props.exercise.sets}</Text>
-        <Text>Reps: {props.exercise.reps}</Text>
-        <Text>Rest: {props.exercise.rest}</Text>
-        <Text>Description: {props.exercise.descr} </Text>
-        <Link to="/edit">
+        <Heading level="5" size="small" margin={{
+          "bottom": "0.8rem", top: "none"
+        }} pad="none"> {props.exercise.name} </Heading>
+      <Text>Sets: {props.exercise.sets}</Text>
+      <Text>Reps: {props.exercise.reps}</Text>
+      <Text>Rest: {props.exercise.rest}</Text>
+      <Text>Description: {props.exercise.descr} </Text>
+      <Box align="center" pad="medium">
+        <Box direction="row" gap="small">
+          <Link to="/edit">
+            <Button
+              href="/edit"
+              icon={<Edit />}
+              margin="small"
+              alignSelf="end"
+              label="Edit"
+              onClick={() => { props.updateExercise(props.exercise) }}
+            />
+          </Link>
           <Button
-            icon={<Edit />}
+            primary
+            href="/edit"
+            icon={<PlayFill />}
             margin="small"
             alignSelf="end"
-            label="Edit"
+            label="Start"
             onClick={() => { props.updateExercise(props.exercise) }}
           />
-        </Link>
+        </Box>
       </Box>
-  </Grommet>
+      </Box>
+    </Grommet >
   );
 }
 
