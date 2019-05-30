117831deb91eeaa0616ba1492e278f9fa9f4f11e
FerrazF <ferrazf9@gmail.com>
2019-05-30 10:50:29 -0400

    Add template styling to workout edit component

--- client/src/containers/editDisplay.jsx
+++ client/src/containers/editDisplay.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Box } from 'grommet';
+import { Box, Grommet } from 'grommet';
+import { hpe } from "grommet-theme-hpe";
 
-
-import Form from "../components/exercise/From.jsx";
+import Form from "../components/exercise/Form.jsx";
 //accepts exercise form components
 function EditDisplay(props) {
-    return (
-      <Box>
-        <Form 
-            handleExerciseFormSubmit={props.handleExerciseFormSubmit}   
+  return (
+    <Grommet theme={hpe}>
+      <Box pad="medium">
+        <Form
+            handleExerciseFormSubmit={props.handleExerciseFormSubmit}
             exercise={props.exercise}
             updateExercise={props.updateExercise}
         />
       </Box>
+    </Grommet>
     );
   }
-  
+
   export default EditDisplay;
