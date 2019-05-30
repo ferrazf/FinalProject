b399ab20365ea7f624128b86ecb7b641b1509a1e
FerrazF <ferrazf9@gmail.com>
2019-05-30 10:48:40 -0400

    Add template styling to index component

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import { Box, Heading, Text } from 'grommet';
+import { Box, Grommet, Heading, Text } from 'grommet';
 import { Link } from "react-router-dom";
-
+import { hpe } from "grommet-theme-hpe";
 
 function Nav(props) {
     return (
-        <Box fill="horizontal">
-            <AppBar >
-                <Link to="/" justify='start'> Pocket Spotter</Link>
-                <Link to="/profile"><Heading level='3' margin='none'> Profile</Heading></Link>
-                {/* Replace with logout if user logged in */}
-                <Link to="/register"> Register</Link>
-                <Link to="/login"> Login</Link>
-                <Link to="/login"> Logout</Link>
-                <Text>{props.name}</Text>
-            </AppBar>
-        </Box>
+        <Grommet theme={hpe}>
+            <Box fill="horizontal">
+                <AppBar >
+                    <Link to="/" justify='start'> Pocket Spotter</Link>
+                    <Link to="/profile"><Heading level='3' margin='none'> Profile</Heading></Link>
+                    {/* Replace with logout if user logged in */}
+                    <Link to="/register"> Register</Link>
+                    <Link to="/login"> Login</Link>
+                    <Link to="/login"> Logout</Link>
+                    <Text>{props.name}</Text>
+                </AppBar>
+            </Box>
+        </Grommet>
     );
 }
 
--- client/src/components/workout/Index.jsx
+++ client/src/components/workout/Index.jsx
@@ -3,45 +3,56 @@ import { Box, Button, Grommet, Text } from "grommet";
 import { Link } from "react-router-dom";
 import { hpe } from "grommet-theme-hpe";
 import { FormView } from "grommet-icons";
+import { CheckboxSelected, PlayFill } from "grommet-icons";
 
 function Workout(props) {
   const start = !props.workout.started_at && (
     <Button
       primary
+      icon={<PlayFill />}
       name={props.workout.workout_id}
       alignSelf="end"
-      label="start"
+      label="Start"
       onClick={props.handleStart}
     />
   );
   const finish = props.workout.started_at && !props.workout.finished_at && (
     <Button
+      primary
+      icon={<CheckboxSelected />}
       name={props.workout.workout_id}
       alignSelf="end"
-      label="finish"
+      label="Finish"
       onClick={props.handleFinish}
     />
   );
 
   return (
+
     <Grommet theme={hpe}>
+      <Box pad="medium" direction="row" gap="small">
+        <Text > {props.workout.name.join(', ')} </Text>
+      </Box>
       <Box
-        pad="medium"
+        align="center"
+        pad="small"
         border={{
           color: "border",
-          side: "all"
+          side: "bottom"
         }}
       >
-        <Text > {props.workout.name.join(', ')} </Text>
-        <Button
-          href="/workout"
-          icon={<FormView />}
-          alignSelf="start"
-          label="View"
-          onClick={() => { }}
-        />
-        {start}
-        {finish}
+        <Box pad="medium" direction="row" gap="small">
+          <Link to="/workout">
+            <Button
+              icon={<FormView />}
+              alignSelf="start"
+              label="View"
+              onClick={() => { }}
+            />
+          </Link>
+          {start}
+          {finish}
+        </Box>
       </Box>
     </Grommet>
   )
