12238a9b00fee97c2839f5bbeff59f4ad6fb6e66
kobilee <kobiatlaslee@gmail.com>
2019-05-31 18:27:21 -0400

    add back button

--- client/src/containers/addDisplay.jsx
+++ client/src/containers/addDisplay.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Box, InfiniteScroll } from 'grommet';
-
+import { Box, InfiniteScroll, Button, Grommet } from 'grommet';
+import { Link } from "react-router-dom";
 import MuscleGroup from "../components/exercise/MuscleGroupSelect.jsx";
 import Add from "../components/exercise/Add.jsx";
-
+import { hpe } from "grommet-theme-hpe";
 //accepts selectMuscleGroup compenent and exercise add components 
 
 function  addDisplay(props) {
@@ -16,16 +16,25 @@ function  addDisplay(props) {
     />
   ))
     return (
-      <Box>
-        <MuscleGroup
-            currentMG={props.currentMG}
-            muscle={props.muscle} 
-            updateMG={props.updateMG}
-        />
-        <InfiniteScroll items={items}>
-            {(item) => (item)}
-          </InfiniteScroll>
-      </Box>
+      <Grommet theme={hpe}>
+        <Box>
+          <MuscleGroup
+              currentMG={props.currentMG}
+              muscle={props.muscle} 
+              updateMG={props.updateMG}
+          />
+          <InfiniteScroll items={items}>
+              {(item) => (item)}
+            </InfiniteScroll>
+            <Link to="/workout">
+              <Button
+                alignSelf="center"
+                label="Back"
+                onClick={() => {}}
+              />
+            </Link>
+        </Box>
+      </Grommet>
     );
   }
   
